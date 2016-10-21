7d2acacc7836b5134aab931271119dfcc2b7ae4b
Faridh Mendoza <faridh@intangible.mx>
2016-10-21 15:13:01 -0500

    Fixes wrong file name for vars

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ENVS=$(cat user-data.prod.txt | awk -F '[ ]' '{printf "-e " $1 " "}')
+ENVS=$(cat user-data.txt | awk -F '[ ]' '{printf "-e " $1 " "}')
 docker-compose run $ENVS worker python main.py
